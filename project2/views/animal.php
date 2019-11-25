97a0e668028442fbe1fc439c42bf8d5d6d6d7bc2
szpatrik98 <43348587+szpatrik98@users.noreply.github.com>
2019-11-25 22:04:47 +0100

    Add files via upload

--- project2/views/animal.php
+++ project2/views/animal.php
@@ -23,47 +23,84 @@ A katonai kutyák számos feladatot végeznek, ideértve a közúti bombák kisz
 </div>
 
 <div class="page">
-    <h1>Állat teszt cím szöveg.</h1>
-    <h2>Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. </h2>
+    <h1>Miért éri meg kis üzletben árusítani a mézet? Kizárólagosság, vagy sem</h1>
+    <h2>A mézet egy kis üzletben árusítja. Egyik nap az üzletvezető azt mondja önnek, hogy egy másik mézszállítót is be fog vonni, hogy legyen választási lehetőség. Gyorsan át kell gondolnia a lehetőségeket, és bebizonyítani, hogy miért nem kellene ezt tennie, miért önnek kellene az egyetlen beszállítónak maradnod. De adunk ötletet arra is, ha be szeretne jutni egy kis üzletbe.
+Mit mondjon, ha konkurenciát kap:
+
+• A vásárló zavarodottsága. „Ha két termék van, a vásárló azon elmélkedik, hogy melyiket kellene választania. Mivel egyiket sem tudja megkóstolni, nem tudja megítélni az ízeket. Lehet, hogy az ár alapján dönt, de ebben valószínűleg nincs olyan nagy különbség. Emellett az eltérő árak csak összezavarják a vásárlót. Azon tűnődik, hogy az egyik méz miért kerül 50 centtel kevesebbe? Tehát egyszerűsítsük a dolgokat, és minimalizáljuk a vásárló zavarát.”
+
+• Kisebb polchely. „2,4 m helyett 1,2 m polcszélességet használsz. Be lehet hozni más terméket, mint a juharszirup vagy a vörös áfonyás-csokoládés szeletek, és nagyobb forgalmat elérni.
+
+Néhány vásárló mézet akar. Más vásárlók juharszirupot akarnak. Miért ne lenne hely mind a két termék számára?
+A kiskereskedelem alapszabálya a hely megfelelő kihasználása. Emellett a választás akkor működik, ha igazi választékot tudunk nyújtani. A legtöbb vásárló számára a termelői méz üvegekben gyakorlatilag ugyanolyannak tűnik.”
+
+ </h2>
+ <h2>
+ • A termék kifogy. „Ha azért hozol be új szállítót, mert tavaly egy pár hónapig nem volt a termékből, megígérem, hogy ez soha többet nem fog előfordulni. Megtettem a megfelelő lépéseket a folyamatos ellátás érdekében. Megnöveltem a kaptárok számát. Csökkentettem az ügyfeleim számát, hogy jobban tudjak koncentrálni. Vásároltam egy tárolótartályt, hogy legyen tartalék készlet. Végül pedig, te vagy az első számú ügyfelem, és sohasem foglak cserbenhagyni. Úgyhogy ne aggódj. Többet sosem fogsz kifogyni a mézből.”
+ </h2>
+ <h2>
+ • Egyéb méztermék. „Ha a másik beszállító olyan terméket ígér, ami nekem nincs – például lépes mézet –, tájékoztass erről, és mindent megteszek, hogy biztosítsam ezt a mézterméket is.
+ </h2>
+ <h2>
+ Tudod, mi (méhészek) mind barátok vagyunk. És mindig szoktunk termékeket cserélni. De őszintén, ezekből a méztermékekből kevesebbet lehet eladni. A folyékony méz az, amit a vásárlók akarnak.”
+ </h2>
+ <h2>
+ • Extra marketing ajánlat. „Ha beleegyezel abba, hogy nem vonsz be másik szállítót, extra szolgáltatást fogok garantálni. Egyszer egy hónapban, az általad kijelölt napon, bejövök egy-két órára és egy kis mézkóstolót és tájékoztatást fogok tartani. Beszélni fogok a méz egészségre gyakorolt jótékony hatásairól. Lehetőséget fogok adni az embereknek arra, hogy több üvegemből is mézet kóstoljanak. Válaszolni fogok a kérdésekre, mivel tudom, hogy a méhek fontos témának számítanak manapság. Megteszem ezt neked, mivel szeretném, ha én maradnék az egyetlen mézbeszállítód.”
+ </h2>
+ 
+ <h2>
+ Megkeres egy üzletvezetőt, hogy árulja a mézét. Ő odaviszi egy polchoz, és azt mondja, hogy már árul mézet. „Miért akarnék egy másik fajtát?” – kérdezi.
+ </h2>
+ <h2>
+ • Választék. „Ha kétféle mézed van, akkor választási lehetőséget kínálsz. A változatosság az élet sója. Ráadásul, ha több terméket kínálsz, az azt mutatja, hogy haladsz a korral, mivel manapság mindenki a választás lehetőségét akarja. Például, látom, hogy a beszállítód műanyag flakonokat használ. Én a mézet üvegben szállítom. Ez egy sokkal hagyományosabb termék. Néhány vásárlód, aki nem szereti a mézet műanyag flakonokban, biztosan nagyon örülne, ha üveges mézet vehetne. Ami a lényeg, a mézeladásod megnőne, és ez jó üzlet.”
+ </h2>
+ <h2>
+ • Igény a különleges mézre. „A vásárló tudja, hogy a mézek különböznek, attól függően, hogy hol tartották a méheket, és mikor pergették a mézet, így nagyobb választási lehetőséget kell nekik adni.
+ </h2>
+  <h2>
+ Sok vásárló kipróbálna egyet, majd egy másikat, vagy akár mindegyikből venne, és eldöntené, hogy melyiket szereti jobban. A választás biztosítása megéde­síti a terméket. Talán nem öt mézfajta, de kettő mindenképpen nagy különbséget jelentene. A változatosság biztosítása sokkal inkább tesz mézforgalmazóvá.”
+ </h2>
+  <h2>
+ Nem fogy ki a termék. „Két beszállítóval sosem fogynál ki a készletből. Borzasztó azt mondani valakinek, aki bejön és mézet szeretne venni, hogy: »Sajnáljuk, de jelenleg nincs készleten. A beszállító azt mondta, hogy néhány héten belül lesz méz.« Egyetlen kereskedő sem szeret csalódást okozni a vásárlóknak, és két beszállítóval nem is fogsz. Nehéz idők járnak a méhészekre. A varroa atkával, a légcsőatkával, a nyúlós költésrothadással és a kaptárelhagyással küzdve, nehéz a méhcsaládokat életben tartani. Ha még nem történt meg, akkor a jövőben biztosan lesz olyan, hogy a beszállítód nem tud mézet adni.
+ </h2>
+  <h2>
+ Pár hónappal ezelőtt jártam itt és nem volt a félkilós üvegből a polcon, ami a legnépszerűbb termék. Miért ne biztosítanád be magad a folyamatos ellátással? Ha az egyik beszállító nem tud mézet szállítani, a másik majd tud.”
+ </h2>
+  <h2>
+ Termékválaszték. „Látom, hogy a beszállítód nem kínál krémmézet. A krémméz kristályosodott állagú méz, mely selymes, krémes, amit egy különleges eljárással lehet elérni. Néhány vásárló az ilyet jobban kedveli a folyékony méznél az íz és a könnyebb kenhetőség miatt is. Én rendszeresen kínálok ilyen terméket. Észszerű többféle méztípust forgalmazni, mivel ez szélesebb választékot nyújt.”
+
+
+ </h2>
+  <h2>
+ Extra szolgáltatás. „Ha behozod az én termékemet is, megígérem, hogy bármely hónapban bejövök, és kicserélem a kristályosodó mézet teljesen folyékony mézre. Mivel a méz kristályosodása természetes folyamat, rövid időn belül már láthatók a kis szemcsék, amelyek gyorsan nagy csomókká állnak össze. Nézd meg a polcon levő termékeket. Sokban vannak nagyobb csomók. Sok vásárló, lényegében a többség, teljesen folyékony mézet szeretne. A folyamatos ellen­őrzéssel biztosítani fogom, hogy az én folyékony mézemben sose alakuljanak ki nagy csomók.”
+ </h2>
+ <a href="https://magyarmezogazdasag.hu/2019/11/23/miert-eri-meg-kis-uzletben-arusitani-mezet-kizarolagossag-vagy-sem">Miért éri meg kis üzemben arusítani mézet.</a>
 </div>
 
 <div class="page">    
-    <h1>Állat teszt cím szöveg.</h1>
-    <h2>Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.</h2>
+    <h1>Tízezrek tiltakoztak a medvék élőhelyére áthelyezett szilveszteri koncert miatt Zakopanéban</h1>
+	<img src="pictures/animal/animal1.jpg" >
+    <h2>Zakopanéban
+Tavaly egy medve elpusztult, mert felébredt a szilveszteri mulatság zajától. A nemzeti park is figyelmeztetett, hogy a zaj és a tűzijáték fényei megijeszthetik az állatokat.
+Több tízezren írták alá a petíciót a lengyel síparadicsomban, Zakopanéban, amelyben az ellen tiltakoztak, hogy a város megváltoztatta a hagyományos szilveszteri koncert és tűzijáték helyszínét. A tiltakozók szerint a városon kívüli hegyoldalra helyezett rendezvény megzavarhatja a medvék téli álmát.
+
+A város lakói egy helyi újságból tudták meg, hogy a városi tanács októberben megtárgyalta a lengyel köztévé által is közvetített ünnepség áthelyezését a belvárosból a hegy lábához, a Tátrai Nemzeti Parkba, a híres Wielka Krokiew síugrósánchoz.
+
+Miután a polgármester elismerte, hogy ilyen döntés született, múlt szombaton több száz tüntető vonult végig a városon egy fekvő medvebábút cipelve. A tiltakozók azt követelték, hogy a polgármester vonja vissza a döntést.
+
+Az újság által indított petíciót keddre már több mint 27 ezren írták alá.
+A szilveszteri rendezvényt élőben közvetítő TVP közszolgálati tévécsatorna "ál-környezetvédőnek" nevezte a tiltakozókat, és azzal érvelt, hogy a sportrendezvényeket is gond nélkül lebonyolítják a térségben.
+
+Az újság cikkét követően
+
+a nemzeti park is közleményt adott ki, amelyben hangsúlyozták, hogy a zaj és a tűzijáték fényei megijeszthetik az állatokat, ami miatt elvándorolhatnak és hosszú távon a populációjuk is csökkenhet. A közlemény rámutatott, hogy tavaly elpusztult egy medve, amely felébredt a szilveszteri mulatság zajától.
+
+A park szakemberei elmondták, hogy a hegyen kárpáti barna medvék élnek, bár azt, hogy mekkora a populáció, nem tudják pontosan.
+
+Az AFP jelentése szerint a zakopanei polgármester pénteken azt mondta, hogy engedtek a közvélemény nyomásának, és a köztévével megegyezve idén is az óvárosban rendezik meg a szilveszteri koncertet.
+</h2>
+<a href="https://www.hirstart.hu/allatvilag.php">Tiltakozas a Medvékért.</a>
 </div>
 
 <?php include_once "_footer.php"; ?>
+
