d4846f3e2744f6ce5febf058c83bafe10798337c
szpatrik98 <43348587+szpatrik98@users.noreply.github.com>
2019-11-25 22:25:03 +0100

    Gyerek Hírek.

--- project2/views/kids.php
+++ project2/views/kids.php
@@ -1,70 +1,67 @@
 <?php if (!defined('APP_VERSION')) { exit; } ?>
 
 <?php include_once "_header.php"; ?>
-<div class="page">
-    <h1>Gyerek teszt cím szöveg.</h1>
-    <h2>Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.</h2>
+<div class="page" >
+   <h1>ÍGY NEVELJ OPTIMISTA GYEREKET.</h1>
+
+	
+     <h2>Gyerekeink meglehetősen ijesztő világban nőnek fel. Egyik válság jön a másik után: bolygónk pusztulása,a mentális problémák, depresszió és szorongás folyamatos növekedése, terrorcselekmények, és még folytathatnánk a sort.Ilyen körülmények között nehéz mindig pozitívan látni a világot. Pedig ha valaha,akkor most valóban szükségünk van optimizmusra.Az emberek természetükből adódóan hajlamosabbak optimistán vagy pesszimistán állni a dolgokhoz, de tudnunk kell, hogy szülőként sokat tehetünk annak érdekében,hogy ösztönözzük és erősítsük a gyerekeink optimizmusát.		
+		# Nem a tökéletesség a cél<br/>	
+		<p><img src="pictures/kids/child1.jpg" style="float:right"></p>
+		A gyerekek nagyon gyorsan le tudnak tenni azokról a dolgokról, amik nem sikerülnek elsőre. Erre hajlamosak vagyunk azt mondani, hogy gyakorlat teszi a mestert, a sok gyakorlás azonban nem feltétlenül vezet tökéletességhez.Az viszont tény, hogy minél többet gyakorolunk valamit, annál jobban fog menni. Ha tehát a gyerek el van kenődve, mert nem megy a matek,vagy mert az általa rajzolt macska a legkevésbé sem hasonlít a kedvenc plüss Cirmijére, akkor ahelyett, hogy a tökéletes eredményt tűzzük ki célul, hozzunk fel korábbi példákat. Rengeteg olyan eset volt már a gyerekek életében is,amikor valami nem sikerült elsőre, de végül csak megtanulta bekötni a cipőjét, már magának készíti a reggelit, és olvasni is egyre jobban tud.Ezek mind olyan dolgok, amiket gyakorlás során tanult meg, akkor is, ha még nem mennek tökéletesen.					
+</h2>
+
+<h2>
+# Az optimizmus nem hamis remény<br/>
+
+Lehet, hogy ez most a legkevésbé sem tűnik pozitívnak, de ne erőltesd az optimizmust a gyerekre. Ugyanis az optimizmus valójában reális gondolkodást igényel, nem pedig pozitívat. Így tudnak a gyerekek felkészülni azokra a dolgokra, amelyekkel szembe kell nézniük. Éppen ezért sokat segít, ha elismerjük a rájuk váró kihívásokat ahelyett, hogy automatikusan azzal próbálnánk nyugtatni őket, hogy minden rendben lesz. Ilyen kihívás lehet egy új környékre költözés vagy a beilleszkedés egy új iskolába. Időbe fog telni, amíg a gyerek megtalálja a helyét és új barátokat szerez, sőt lehet, hogy addig még csalódások is fogják érni. A legtöbb, amit tehetsz, ha őszintén beszélgetsz vele az őt foglalkoztató dolgokról, támogatod a céljai elérésében.
+</h2>
+                
+       
+		<a href="https://divany.hu/szuloseg/2019/11/21/optimista-gyerek/"> OPTIMISTA-Gyerek </a>
+		
 </div>
 
 <div class="page">
-    <h1>Gyerek teszt cím szöveg.</h1>
-    <h2>Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. </h2>
+    <h1>ÍGY TESSZÜK TÖNKRE A GYEREKEINK GYEREKKORÁT</h1>
+    <h2>Mi a baj a mostani gyerekekkel? Egyrészt semmi. Már sok ezer éve busonganak idősödő felnőttek a fiatal generáció kapcsán, hogy jaj, ezek a mai fiatalok… Másrészt viszont tényleg van valami, amivel mi, felnőttek a mai társadalmi elvárásoknak megfelelve szerencsétlenné tesszük a gyerekeinket, és amit nem ártana szem előtt tartanunk.
+		A gyerekek és fiatalok pszichés állapota az utóbbi évtizedben drasztikusan romlott. Amerikai adatokat fogok idézni (a The New York Timestól kölcsönözve), melyek nem azonosak a magyar adatokkal, de a tendenciák nálunk is megjelenhetnek. 2009 és 2017 között a kamaszkori depresszió előfordulása 60 százalékkal nőtt. A szorongás a mai tinédzserek körében nagyobb, mint a hidegháború idején. 2007 és 2015 között megduplázódott azoknak a tinédzsereknek a száma, akik sürgősségi ellátásba kerültek öngyilkossági gondolatok vagy kísérletek miatt.
+
+A gyerekeink nincsenek jól. Kim Brooks, aki szülői témákban ír cikkeket és könyvet, maga is anya. Régóta keresgéli, hogy mi lehet az az egy ok, ami a gyerekek rossz közérzetét, lelki problémáit okozza. A túl sok képernyő előtt töltött idő? A kevés játék a szabadban? A sok különóra? Az ételek? Arra jutott, hogy mind igaz, de önmagában egyik sem.
+Magukra hagyott szülők<br/>
+Azt mondja, sosem voltak annyira egyedül a szülők a gyerekek nevelésében, mint manapság. Míg régen tényleg ott volt egy egész falu egy gyerek neveléséhez, addig ma a szülők szinte csak magukra számíthatnak. A munkaidő, a munkahelyen töltött órák száma megnőtt, emellett a szülőknek a gyerekek fizikai biztonságát sokszor előtérbe kell helyezniük a megfelelő szociális környezet biztosítása helyett. Vagyis az óvodákat, iskolákat ma arra használjuk, hogy biztonságban őrizzék a gyerekeket, amíg le nem jár a munkaidőnk, vagyis képesek nem leszünk mi is vigyázni rájuk. De az iskola, óvoda is egyre szigorúbb időbeosztást ad a gyereknek, egy iskolában töltött nap során az ebédidő egyre rövidül, szabad játékra, lógásra, udvaron létre alig jut idő – a délutánok is külön foglalkozásokkal, gondosan beszabályozott, „hasznos” elfoglaltságokkal, órákkal vannak teli.
+	</h2>
+	<img src="pictures/kids/child2.jpg" >
+	<h2>A legszomorúbb statisztika<br/>
+A gyerekek és fiatalok szomorú öngyilkossági próbálkozásai más időbeli eloszlást mutatnak, mint a felnőtteké. A felnőttek között az öngyilkossági kísérletek a nyári, meleg hónapokban tetőznek, a gyerekeknél az iskola időszakára jellemzők. Akkora stresszt és nyomást élnek meg a gyerekek az iskolában, hogy itt érzik úgy, nem tudnak megbirkózni vele. És sajnos már a nyári hónapok sem adják meg a kötetlen idő lehetőségét: táborokba, újra szigorú napirendű programokra hordjuk a gyerekeket – megint csak nem rossz szándékból, de hát nekünk nyáron is dolgozni kell, hova is tennénk őket két és fél hónapig?
+	
+	</h2>
+	<a href="https://divany.hu/szuloseg/2019/11/19/tonkretett-gyerekkor/"> Tönkretett-Gyerekkor </a>
 </div>
 
 <div class="page">    
-    <h1>Gyerek teszt cím szöveg.</h1>
-    <h2>Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.</h2>
+    <h1>4 DOLOG, AMIVEL PORRÁ ZÚZOD A GYEREK ÖNBIZALMÁT</h1>
+    <h2>Ezeket a destruktív nevelési módszereket soha, semmilyen körülmények között nem szabadna alkalmaznia egyetlen szülőnek sem, ha jót akar a gyerekének.
+Önbizalmunk alapvetően meghatározza a viselkedésünket, döntéseinket, boldogulásunkat a magánéletben és a munkában – azaz az egész életünket. Akinek egészséges önbizalma van, az könnyebben veszi az akadályokat, nyitottabb az új dolgok iránt, és sokkal nagyobb eséllyel éri el a céljait, mint azok, akik nem hisznek önmagukban.
+
+Szülőként fontos feladatunk gyerekeink pozitív önértékelésének elősegítése, ám előfordul, hogy egy-egy feszült helyzetet olyan módon kezelünk, amivel éppen ennek ellenkezőjét érjük el: az indulat szülte nevelési eszközök bizony egy csapásra lerombolhatják a gyerek önbizalmát. A Psychology Today cikke négy olyan destruktív nevelési módszerre hívja fel a figyelmet, amit soha, semmilyen körülmények között nem szabadna alkalmaznia egyetlen szülőnek sem, ha jót akar a gyerekének.
+		1. Kiabálás és testi fenyítés<br/>
+		<p></p>
+Ez a legrosszabb, amit gondozóként tehetünk egy gyerekkel, és bár 2005 óta Magyarországon is törvény tiltja a gyerek bántalmazását, nem nehéz elképzelni, hogy a valóságban otthon, a négy fal között bizony elő-előfordul. Márpedig akár szavakkal, akár fizikailag verjük a gyereket, azzal nemcsak azt tanítjuk neki, hogy a gyengébbel szemben oké így érvényesíteni az akaratunkat, de egy pillanat alatt porrá zúzzuk az önbizalmát. Ilyen káros és legfeljebb rövid távon működő stratégiák helyett arra kellene törekedni, hogy higgadtan, türelmesen megbeszéljük a gyerekkel az adott problémát, és igyekezzünk együtt megoldást találni rá.
+
+        2. Múltbeli ballépések felemlegetése<br/>
+Nem sok értelme van egy már régen elrendezett nézeteltérést vagy hibát újra és újra felhozni, hiszen egyrészt mindenkinek joga van egy ilyen után tiszta lappal indítani, másrészt ezzel csak azt erősítjük meg a gyerekben, hogy a haragtartás is teljesen rendben van. Ha ehelyett az erősségeire, a pozitív viselkedéseire fókuszálunk, akkor ő is sokkal jobban fogja érezni magát a bőrében, és ezáltal annak is jóval csekélyebb esélye lesz, hogy megismétlődik egy-egy rossz döntése.
+		</h2>
+		<img src="pictures/kids/child3.jpg" >
+		<h2>
+		3. Bűntudatkeltés<br/>
+Az teljesen rendben van, ha megkérdezzük a gyereket, hogy érezné magát XY helyében vagy egy adott helyzetben, például ha az ő játékát vennék el szó nélkül, vagy ha neki kellene szembesülnie azzal, hogy a szomszédok rajtakapták a gyerekét spanglizás közben. Sok szülő azonban hajlamos túltolni egy-egy ilyen kérdést: Van fogalmad róla, mekkora szégyent hoztál a családra? Szerinted mit gondolnak most rólunk a Kovácsék? Ugye tudod, hogy ezzel elvesztetted a beléd vetett bizalmamat?
+
+Márpedig ha nem akarjuk teljesen elidegeníteni magunktól a gyereket, a bűntudatkeltés és kontroll helyett inkább megértő, támogató kommunikációt érdemes alkalmazni: ha a szülő nem számon kér, hanem az egóját félretéve próbál beszélgetni, akkor sanszos, hogy a gyerek őszintén meg is fog nyílni neki.
+		4. Szarkazmus<br/>
+„Mondhatom, ez okos húzás volt.” „Látom, növeszted a beled. Nagyon ügyesen csinálod, csak így tovább!” Az ilyen gúnnyal teli, akár megvetéssel is felérő beszólások nemcsak a szülő és a gyerek közötti kapcsolatot lehetetlenítik el, de olyan fokú szégyent kelthetnek a gyerekben, amit még felnőttkorában is nyögni fog. Nem éri meg.
+		</h2>
+		<a href="https://divany.hu/szuloseg/2019/11/11/gyerek-onbizalom/"> Gyerek-Önbizalom </a>
 </div>
 
 <?php include_once "_footer.php"; ?>
