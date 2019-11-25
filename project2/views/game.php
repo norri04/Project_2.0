44d1361247e2f46cddfba6a93f563b34e62a7b8b
szpatrik98 <43348587+szpatrik98@users.noreply.github.com>
2019-11-25 22:10:18 +0100

    Add files via upload

--- project2/views/game.php
+++ project2/views/game.php
@@ -1,26 +1,42 @@
 <?php if (!defined('APP_VERSION')) { exit; } ?>
 
 <?php include_once "_header.php"; ?>
+<?php include_once "_header.php"; ?>
 <div class="page">
-    <h1>Játék teszt cím szöveg.</h1>
-    <h2>Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.</h2>
+    <h1>A világ legjobbjai között van virtuális Forma 1-ben egy magyar e-sportoló.</h1>
+    <h2>Bereznay Dániel még csak 19 éves, de már most az egyik legügyesebb e-versenyzőnek számít a szcénában.</h2>
+	<img src="pictures/games/game1.jpg" >
+	<h2>
+	Néhány kivételtől eltekintve sajnos nincsen túl sok olyan játékos, aki tudásának köszönhetően nemzetközi szinten is képes lenne kis hazánkat képviselni. Persze azért akadnak kivételek, és a Formula-1 hivatalos virtuális bajnokságában is találni egy tehetséges magyar srácot.
+
+Hirdetés
+Friss interjút közölt az Index a 19 éves Bereznay Dániellel, aki már nagyon régóta foglalatoskodik autóversenyzős játékokkal, és egy ideje már professzionális szinten űzi a Codemasters F1-játékait. Mindössze 12 éves volt, amikor is sikerült megnyernie egy magyar bajnokságot, ráadásul 14 évesen már orosz és német ligákban is próbára tette tudását. Korábban a Mercedes is felfigyelt az eredményeire, aminek köszönhetően a tavalyi szezonban a vállalatot képviselve mérethette meg magát a Formula 1 Esports Series bajnokságban. A 2018-as szezonban sikerült is a második helyen végeznie.
+
+Bereznay jelenleg az Alfa Romeo versenyzőjeként tevékenykedik, és saját állítása szerint heti 60-70 órát gyakorol, hogy formában maradjon, és nem is igazán játszik mással, nehogy kiessen a gyakorlatból. Véleménye szerint egyébként szépen fejlődik a Codemasters játéksorozata, ami egyre élethűbbé válik, így pedig folyamatosan közelebb kerül egy valódi szimulátor szintjéhez.
+
+Ha még több részletet szeretnétek megtudni a fiatal versenyző karrierjéről, illetve a virtuális Forma-1 működéséről, akkor itt megtaláljátok a teljes interjút. Ti mennyire követitek figyelemmel a Formula 1 Esports Series alakulását?
+	</h2>
+	<a href="https://www.gamestar.hu/esport/formula-1-esports-series-bereznay-daniel-270835.html">Link az eredeti cikkhez..</a>
+</div>
+
+<div class="page">
+    <h1>Devil’s Hunt teszt - a laza srác meg a pokolfajzatok.</h1>
+    <h2>Nem a szokásos hősmesét regéli el nekünk a Devil's Hunt, ami még rendben is van, hiányosságai azonban csúnyán belerondítanak az összképbe. A történet főhőse Desmond, aki egy viszonylag átlagos figura, leszámítva, hogy menő kecója van Miamiban, alkalmanként pedig mindenféle furcsa arccal bokszol. A cselekmény picit lassan indul be, de aztán hirtelen minden a szerencsétlen fickó nyakába szakad, aki a munkahelyi és magánéleti problémákat nem túl jól kezelve egyszerűen lehajt kocsijával egy hídról, egyenesen bele a vízbe. Az öngyilkosságot követően felgyorsulnak az események, Desmond a pokolba kerül, egyezséget köt magával Luciferrel, majd démoni erőkkel felvértezett bosszúállóként tér vissza az élők sorába.
+	A Devil's Hunt egy szimpla akció-kalandjáték, játékmenete nagyon hasonlít műfajtársaiéhoz. Ez röviden annyit tesz, hogy ha épp nem kocogunk vagy objektumokat használunk a pályákon, akkor az egér gombjait vadul csapkodva verjük bucira az életünkre törő démonokat és egyéb rosszfiúkat. Desmond ugyan megtartotta emberi külalakját, ám harc közben karjai ördögi formát öltenek, és ha úgy hozza a helyzet, főhősünk akár teljes testében pokoli szörnyeteggé tud változni. A folyamatos ütlegelés olykor fárasztóvá és monotonná válik, így nagy szerencse, hogy különleges képességeket is be tudunk vetni harc közben, amelyekkel kihúzhatjuk magunkat a szorult helyzetekből. A történet és a játékmenet kombinációja teljesen korrekt lenne, hiszen az angyalok és démonok örökös harca mindig jó alapanyag, a Darksidersre hajazó vagdalkozás pedig ideig-óráig szórakoztató marad. Ám ahogy azt már számtalanszor láthattuk, még egy remek alapot is képes tönkretenni a silány kivitelezés.
+	</h2>	
+	<h2>
+	Devil May Cry Light
+	</h2>
+	<img src="pictures/games/game4.jpg" >
+	<h2>
+	Mielőtt teljesen leírnánk a játékot, érdemes figyelembe venni, hogy a Layopi Games elég friss stúdió, ez a srácok első címe, így értelemszerűen nem igazán várhatunk AAA minőséget tőlük. Ez picit enyhít a helyzeten, ám vajmi kevés vigaszt nyújt, amikor elénk tárulnak a szinte fapofával kiabáló karakterek, és meghalljuk a nem túl nagy beleéléssel előadott szinkront. Desmondból valószínűleg egy Devil May Cry-os Dantét akartak faragni a fejlesztők, ám néhány kivételtől eltekintve nagyon gyenge szövegeket írtak szegénynek, és a menőnek szánt beszólások sem mindig menők. Főleg, ha rezzenéstelen arccal mond valami frappánsnak vélt dolgot a karakter. A körítés hiányosságain még csak-csak túltehetjük magunkat, de az olykor megakadó, nem túl pörgős harc már elég csúnyán rányomja bélyegét a játékélményre.
+	</h2>
+	<img src="pictures/games/game3.jpg" >
+	<h2>
+	Nem pokolian rossz
+Szerencsére azért vannak pozitívumok is, mert például a démonok korrekt dizájnt kaptak, és a felcsendülő zenékkel sincs különösebb probléma, habár előfordult, hogy nem illett az adott szituhoz egy muzsika. A sztori viszonylag rendben van, ami nem csoda, hiszen egy fantasyregény az alapja, úgyhogy ha nem zavarnak az ügyetlen megoldások, akkor megérhet egy próbát a Devil's Hunt. Persze csak, ha már tényleg nincs mivel játszanotok.
+	</h2>
+	<a href="https://www.gamestar.hu/hir/devils-hunt-teszt-270632.html">Link az eredeti cikkhez..</a>
 </div>
 
 <div class="page">
