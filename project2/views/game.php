e8ca5a390eb2a24502791864e11aabaf7133693b
szpatrik98 <43348587+szpatrik98@users.noreply.github.com>
2019-11-25 22:14:36 +0100

    Add files via upload

--- project2/views/game.php
+++ project2/views/game.php
@@ -39,48 +39,48 @@ Szerencsére azért vannak pozitívumok is, mert például a démonok korrekt di
 	<a href="https://www.gamestar.hu/hir/devils-hunt-teszt-270632.html">Link az eredeti cikkhez..</a>
 </div>
 
-<div class="page">
-    <h1>Játék teszt cím szöveg.</h1>
-    <h2>Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. </h2>
-</div>
-
 <div class="page">    
-    <h1>Játék teszt cím szöveg.</h1>
-    <h2>Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. 
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.</h2>
+    <h1>Star Wars Jedi: Fallen Order előzetes - galaxis útikalauz padavanoknak.</h1>
+    <h2>Néhányan már kipróbálhatták a jövő hónapban megjelenő, várva várt Fallen Order demóját, és a tapasztalatok alapján hosszú éhínség után talán végre egy remek Star Wars játékkal lehet dolgunk.
+Mióta az EA-nél pihennek a kizárólagos jogok a Star Wars játékok készítésére, egyre inkább az volt az érzésünk, hogy talán már soha nem virágzik fel újra az a kor, ami kitermelte magából a a Jedi Knight szériát vagy a két Knights of the Old Republic játékot. Noha egyelőre tendenciáról kár lenne beszélni, de úgy tűnik hosszú évek után végre ismét egy király videojáték születhet a messzi-messzi galaxisban, ami ha nem is lesz forradalmi, reményt adhat arra, hogy nem csak a multis lövöldéknek van helye a piacon.
+Az EA nemrégiben megengedte néhány külföldi lapnak és videósnak, hogy 3 órán keresztül játszhassanak a november 15-én érkező Star Wars Jedi: Fallen Orderrel és a legtöbb vélemény határozottan pozitív képet fest a Respawn alkotásáról. Ha videón néznétek meg a gameplay-t, akkor ide kattintva minden elérhető, de még mesélnék azért kicsit arról, amit jelenleg tudni lehet.
+
+	</h2>
+	<h2>
+	Volt egyszer egy 66-os parancs
+	</h2>
+	<h2>
+	A történet A Sith-ek bosszúja és az Egy új remény között játszódik; a birodalom napról napra erősebb lesz, a kegyetlen tisztogatás után megmaradt jedik bujkálni kényszerülnek, hiszen galaxisszerte portyáznak az Uralkodó különlegesen képzett inkvizítorai, akik minden Erő-érzékeny személyre lecsapnak, akit sikerül felkutatniuk. Főhősünk, Cal Kestis szerencsére nem egy fogalmatlan kezdő, átesett a jedi tréningen, ugyan a Jedi Lovag címet nem szerezte meg, de mind a fénykarddal, mind az Erővel remekül bánik. Callal egy Jedi mester nyomába eredünk, aki egy birodalom ellen felhasználható fegyvert szeretne felkutatni, eközben pedig belebotlunk a Zsivány egyesből és a Klónok háborújából is ismert Saw Gerrera vezette lázadók csoportjába is.
+	</h2>
+	<img src="pictures/games/game5.jpg" >
+	<h2>
+	Cal komoly kiképzést kapott, ezt pedig remekül implementálták a játékmenetbe. A küldetések során szerzett tapasztalatpontokat újabb trükkökre költhetjük el, ami pontosan illik a sztoriba; amikor feloldunk egy új mozdulatot vagy Erőképességet, akkor a főhősünk visszaemlékezik a tanításokra, amit a mestereitől kapott, és amiket a bujkálás éveiben minél mélyebbre temetett magában a túlélés érdekében. 
+	</h2>
+	<h2>
+	Star Souls
+	</h2>
+	<h2>
+	És bizony szükség is lesz az újabb képességekre, ugyanis a birodalom és az utunk során felfedezésre váró bolygók bizony nem egy veszélyt tartogatnak számunka. Az már ez első játékmenet előzetesekből kitűnt, hogy a készítők a rengeteget merítettek a Soulsborne játékokból, ezt ők maguk sem tagadják. A harcok bizony nem kis fejtörtést okoznak majd azoknak, akik azt gondolják, hogy egy fénykardtól máris halhatatlanok. Hogy az ellenség fölé kerekedjünk elengedhetetlen lesz, hogy képességeinket, hárításainkat és kitéréseinket legalább annyira gyakran és jó ütemben használjuk, mint a kezünkben tartott jedi-szablyát.
+
+Eleinte bizony nem lesz könnyű dolgunk, hiszen az Erőnek csak egy nagyon limitált szeglete áll rendelkezésünkre. A játék korai óráiból származó demóban remekül bemutatkozik, hogy milyen taktikai mélységet adhat akár csak egy új képesség, például az Erő lökés megszerzése. Kezdetben nem hagyatkozhatunk másra csak fénykardunkra, illetve a hárításra és a kitérésre. Miután megszereztük a fent említett képességet, már képesek leszünk lövedékeket és detonátorokat visszalökni a támadóinkra, illetve ledönteni őket lábukról.
+	</h2>
+	<img src="pictures/games/game6.jpg" >
+	<h2>
+	A másik Souls játékokra jellemző aspektus, hogy szintén meghatározott helyeken tudunk majd menteni, amiket itt meditációs pontoknak hívnak, ha elhaláloznánk, akkor ezekhez kerülünk vissza, ráadásul XP-t is vesztünk. Egy újítás azonban, hogy azt az ellenfelet, aki vesztünket okozta, megjelöli a játék, és ha akár csak egy ütést is beviszünk neki, akkor visszakapjuk ellopott pontjainkat. Fontos azért megjegyezni: a nehézségi fokozatokra kiemelten figyelt az EA, nem az a cél, hogy mindenki agybajt kapjon a kihívásoktól, a játékot lehet majd megfelelő kihívások mentén, de könnyített módban is végigvinni, hogy a történeten legyen a hangsúly.
+	</h2>
+	<h2>
+	Csillagok között
+	</h2>
+	<h2>
+	Az akció persze nem pöröghet folyamatosan a maximumon, így a combosabb harcok között platform és puzzle feladatokat oldhatunk meg. Ezek is nagyban függnek a haladásunktól, ugyanis a pályák úgy vannak felépítve, hogy többször is visszatérhessünk hozzájuk, mivel bizonyos részeiket csak akkor érjük el, ha egy adott képesség birtokában vagyunk. A legtöbben kihangsúlyozták, hogy a 3 óra végén úgy érezték, hogy szinte semmit nem láttak a játékból, ez pedig azért lehet, hiszen a legtöbben ezt az időt egy bolygón, a Zeffón töltötték el, melynek közel sem fedezték fel minden részletét, ezen kívül viszont még rengeteg planéta vár felfedezésre - többek között a Dathomir és a Kashyyyk is -, így biztosra vehető, hogy jó pár óránk lesz abban, hogy feltérképezzük messzi-messzi galaxist. 
+	</h2>
+	<img src="pictures/games/game7.jpg" >
+	<h2>
+	Az ügyességi részek kicsit hasonlítanak a Tomb Raider játékokra, azzal a különbséggel, hogy egy jedivel talán még szórakoztatóbb akrobatikus ugrásokat végrehajtani, az erő segítségével a saját igényünk szerint alakítani a platformokat és a környezeti elemeket figyelembe véve lesújtani egy ellenfélre. Ellenlábasból is lesz jó pár féle és végre bővül a kedvenc galaxisunk, hiszen az új bolygók megjelenésével rengeteg új fajt fedezhetünk fel, akikről ezúttal BD-1 névre keresztelt kötelező cuki droid segítőnkön keresztül tudhatunk meg több infót. Ő nem csak a lore-t tekintve bizonyul majd hasznosnak, hiszen bizonyos platform részeknél is hasznát vehetjük majd, valamint a harcokban ki tud segíteni minket kis élettöltőkkel, ha sikerülne több pofonba is belenéznünk egymás után, ami bizony elképzelhető.
+
+Egyelőre keveset láttunk a Star Wars Jedi: Fallen Orderből és az is előfordult már, hogy az EA tett olyan ígéreteket, melyek mögött nem volt fedezet, de a Respawn csapata igazából még nem hagyott minket cserben, és a látottak alapján talán végre megkaphatjuk azt a Star Wars játékot, melyre hosszú évek óta várunk. Ti mit gondoltok?
+	</h2>
+	<a href="https://www.gamestar.hu/hir/star-wars-jedi-fallen-order-demo-elozetes-20191018-269691.html">Link az eredeti cikkhez..</a>
 </div>
 
 <?php include_once "_footer.php"; ?>
