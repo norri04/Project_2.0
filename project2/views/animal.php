6898e70ecd97554ceae1854ce6f85d71d21bfa7f
szpatrik98 <43348587+szpatrik98@users.noreply.github.com>
2019-11-25 22:02:00 +0100

    Add files via upload

--- project2/views/animal.php
+++ project2/views/animal.php
@@ -2,25 +2,24 @@
 
 <?php include_once "_header.php"; ?>
 <div class="page">
-    <h1>Állat teszt cím szöveg.</h1>
-    <h2>Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.
-        Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg. Teszt tartalom szöveg.</h2>
+    <h1>Különleges kapucnis öltözékkel védenék meg a katonai kutyák hallását</h1>
+	
+    <h2>Az amerikai hadsereg dolgozta ki a technológiát, ami megóvhatja az ebek kifinomult hallását.
+	Fejlesztés alatt áll egy új kapucnis öltözet kifejezetten katonai kutyák számára, akik embertársaikhoz hasonlóan gyakran nagy zajnak vannak kitéve. Ez a csúcstechnológiás fejlesztés azonban megvédi majd a kutyák kifinomult hallását – írja a Stars and Stripes.
+
+A hadsereg a Zeteo Tech társasággal együtt dolgozta ki az orvostechnikát, amely a kutyák hallását megóvja majd. Ugyanis a helikopteres műveletek során gyakran vesztenek a hallásukból az ebek, de az új fejlesztés tesztjei azt mutatják, hogy ez kiküszöbölődni látszik.
+
+„Még egy rövid helikopteres repülés is befolyásolhatja a kutya hallását, ami csökkenti a teljesítményét, és képtelen lesz értelmezni a gazdája parancsait, ami akadályozhatja a küldetést"
+
+– mondta Stephen Lee, a az amerikai Hadseregkutatási Iroda tudósa.
+
+A katonai kutyák számos feladatot végeznek, ideértve a közúti bombák kiszimatolását, és az ellenséges harcosok felkutatását is. A halláskárosodás pedig náluk is épp olyan gyakori, mint az emberi egységeknél a hadseregben. Márpedig a halláskárosodás a legelső a katonaságban szerzett fogyatékosságok gyakoriságának listáján.
+	</h2>
+	
+	<h2>
+	Az új öltözet, ami teljesen rásimul a kutyák fejére, kompatibilis a védő szemüvegekkel, és lehetővé teszi, hogy szűk helyeken vessék be a kutyás egységeket. A fejlesztés meghosszabbíthatja a kiképzett kutyák életét, bár még nem tudni mikor vetik be először élesben ezeket.
+	</h2>
+	<a href="https://www.szeretlekmagyarorszag.hu/kulonleges-kapucnis-oltozekkel-vedenek-meg-a-katonai-kutyak-hallasat/">Különleges kapucnis öltözet.</a>
 </div>
 
 <div class="page">
