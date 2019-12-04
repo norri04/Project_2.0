12e868dc964af7a069cfa7f6441d47b0c76f8088
norri04 <norri04@users.noreply.github.com>
2019-12-04 10:46:46 +0100

    Feltöltés korrekció
    A feltöltés még nemérhető el -mert a bejelentkezés sem működik-

--- Project_2.0/project2/views/upload.php
+++ Project_2.0/project2/views/upload.php
@@ -1,1 +1,9 @@
-<?php // itt lehet majd feltölteni
+<?php //A feltöltés itt lesz
+?>
+<?php if (!defined('APP_VERSION')) { exit; } ?>
+
+<?php include_once "_header.php"; ?>
+
+<p>A feltöltés még nem érhető el.</p>
+
+<?php include_once "_footer.php"; ?>
