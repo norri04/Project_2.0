071e4eac4ee28c827f5c51c54d4e2b6db7f9df83
norri04 <norri04@users.noreply.github.com>
2019-12-04 12:41:59 +0100

    nem tudom már mi történik

--- Project_2.0/project2/views/404.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined('APP_VERSION')) { exit; } ?>
-<?php include_once "_header.php"; ?>
-<div>
-    <h1>Oh no!</h1>
-    <p>404 - not found</p>
-</div>
-<?php include_once "_footer.php"; ?>
