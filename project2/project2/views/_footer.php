e74cb621cb1ae143f3506fe9e8a12c7fd98e992e
norri04 <norri04@users.noreply.github.com>
2020-01-15 15:45:33 +0100

    odakerült egy mappa megint

--- project2/project2/views/_footer.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('APP_VERSION')) { exit; } ?>
-    </main>
-    <footer>
-        <div class="container">
-            <p>&copy; 2019 QLA News. All rights reserved.</p>
-        </div>
-    </footer>
-</body>
-</html>
--- project2/project2/views/upload.php
+++
@@ -1,9 +0,0 @@
-<?php //A feltöltés itt lesz
-?>
-<?php if (!defined('APP_VERSION')) { exit; } ?>
-
-<?php include_once "_header.php"; ?>
-
-<p>A feltöltés még nem érhető el.</p>
-
-<?php include_once "_footer.php"; ?>
